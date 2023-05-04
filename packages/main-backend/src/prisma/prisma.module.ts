37e0a5f44609b36e12259e67cd3bd8470d520038
Hanz <haniel56@zoho.eu>
2023-05-05 00:00:06 +0530

    chore: prisma module init

--- packages/main-backend/src/prisma/prisma.module.ts
+++ packages/main-backend/src/prisma/prisma.module.ts
@@ -1,7 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './prisma.service';
 
+@Global()
 @Module({
+  imports: [ConfigModule.forRoot()],
   providers: [PrismaService],
+  exports: [PrismaService],
 })
 export class PrismaModule {}
