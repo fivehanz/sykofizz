a01de46e98bf42fcf8b523c0104d9df740f96c19
Hanz <haniel56@zoho.eu>
2023-05-18 14:51:35 +0530

    chore: admin sidebar init

---
+++ packages/web/app/admin/page.tsx
@@ -0,0 +1,13 @@
+import AdminSidebar from '../../components/admin-sidebar/admin-sidebar';
+import React from 'react';
+
+const page = () => {
+  return (
+    <div>
+      <AdminSidebar />
+      <p>page</p>
+    </div>
+  );
+};
+
+export default page;
