3330d0154f353b95ceeb815ab1eea76809019d04
Hanz <haniel56@zoho.eu>
2023-05-22 21:32:39 +0530

    chore: authform component init

--- packages/admin/components/auth-form/auth-form.tsx
+++ packages/admin/components/auth-form/auth-form.tsx
@@ -3,16 +3,19 @@ import { useForm, SubmitHandler } from 'react-hook-form';
 /* eslint-disable-next-line */
 export interface AuthFormProps {}
 
-export interface IFormInput {
+interface IFormInput {
   email: string;
 }
 
 export function AuthForm(props: AuthFormProps) {
-  const { register, handleSubmit } = useForm<IFormInput>();
-  const submitForm: SubmitHandler<IFormInput> = (data) => console.log(data);
+  // const { register, handleSubmit } = useForm<IFormInput>();
+  const submitForm = (data: any) => {
+    data.preventDefault();
+    console.log(data);
+  };
 
   return (
-    <form className="w-full max-w-md" onSubmit={handleSubmit(submitForm)}>
+    <form className="w-full max-w-md" onSubmit={submitForm}>
       <h1 className="mt-3 text-2xl font-semibold text-gray-800 capitalize sm:text-3xl dark:text-white">
         sign In
       </h1>
@@ -39,15 +42,11 @@ export function AuthForm(props: AuthFormProps) {
           type="email"
           className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
           placeholder="Email address"
-          {...register('email', { required: true })}
         />
       </div>
 
       <div className="mt-6">
-        <button
-          type="submit"
-          className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50"
-        >
+        <button className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
           Sign in
         </button>
       </div>
