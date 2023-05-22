527d3f78fd71bd7f349a9cbfef240a82b072775d
Hanz <haniel56@zoho.eu>
2023-05-22 21:37:14 +0530

    chore: rm old pkgs

--- packages/admin-old/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default async function Index() {
-  return (
-    <div className="flex flex-row justify-center items-center h-80">
-      <h2 className="text-4xl font-bold ">
-        welcome to <span className="text-accent">sykofizz</span>
-      </h2>
-    </div>
-  );
-}
