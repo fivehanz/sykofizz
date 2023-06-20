e4394cc0279cc7907b64e129a4ef8e5ce8d80fb8
fivehanz <22578473+fivehanz@users.noreply.github.com>
2023-06-20 17:35:36 +0000

    chore: jnzi

--- replit.nix
+++ replit.nix
@@ -1,5 +1,6 @@
 { pkgs }: {
     deps = [
+		pkgs.nodePackages.prettier
         # pkgs.esbuild
         pkgs.nodejs-18_x
         pkgs.nodePackages.pnpm
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
 </svelte:head>
 
 <section class="container px-4 my-6 flex justify-center items-center min-w-full">
-	<h1 class="text-3xl font-bold">welcome to sykofizz</h1>
+	<h1 class="text-3xl font-bold">welcome to sykofizz - jnzi</h1>
 </section>
 
 <style lang="postcss">
