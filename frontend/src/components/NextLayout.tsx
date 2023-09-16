3d8352240502fdd62270ceb6fb294bd406dc5c04
Hanz <haniel56@zoho.eu>
2023-09-16 22:01:43 +0530

    chore: add nextui basic layout

---
+++ frontend/src/components/NextLayout.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { NextUIProvider } from "@nextui-org/react";
+import NavBar from "@/components/NavBar";
+
+const NextLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <React.StrictMode>
+      <NextUIProvider>
+        <div className="dark text-foreground bg-background min-h-screen">
+          <header>
+            <NavBar />
+          </header>
+          <main className="px-5 mx-auto">{children}</main>
+          <footer>
+            <p className="px-5 my-4">Footer</p>
+          </footer>
+        </div>
+      </NextUIProvider>
+    </React.StrictMode>
+  );
+};
+
+export default NextLayout;
