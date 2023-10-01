050f7e90a59b14f064795e84920377d14769b251
Hanz <haniel56@zoho.eu>
2023-10-01 10:46:50 +0530

    chore: add wagtail as cms init

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A Nix-flake-based Python development environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [ python311 virtualenv ] ++
+            (with pkgs.python311Packages; [ pip ]);
+          shellHook = ''
+    export PIP_PREFIX=$(pwd)/_build/pip_packages #Dir where built packages are stored
+    export PYTHONPATH="$PIP_PREFIX/${pkgs.python311.sitePackages}:$PYTHONPATH"
+    export PATH="$PIP_PREFIX/bin:$PATH"
+    unset SOURCE_DATE_EPOCH
+'';
+        };
+      });
+    };
+}
